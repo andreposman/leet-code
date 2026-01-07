477fb440ea0429efb2893085d0f9c876b1cd9d04
Andre de Fazzio Posman <andre.posman@mercadolivre.com>
2026-01-06 22:57:58 -0300

    adding hashmap to improve speed and complexity: from O(n2) -> O(n). n = number of entries

--- easy/two-sum/main.go
+++ easy/two-sum/main.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// nums := []int{2, 7, 11, 15}
-	// target := 9
+	nums := []int{2, 7, 11, 15}
+	target := 9
 
 	// nums := []int{3, 2, 4}
 	// target := 6
 
-	nums := []int{3, 3}
-	target := 6
+	// nums := []int{3, 3}
+	// target := 6
 
 	solution := twoSum(nums, target)
 
@@ -18,19 +18,20 @@ func main() {
 }
 
 func twoSum(nums []int, target int) []int {
-	var twoSum []int
-
 	fmt.Println("EASY - TWO SUM")
-	fmt.Println("-------------------")
+	fmt.Println("-------------------------")
 
-	for i := range len(nums) {
-		for j := range len(nums) {
-			if nums[i]+nums[j] == target && i != j {
-				twoSum = append(twoSum, i, j)
-				return twoSum
-			}
+	seen := make(map[int]int)
+
+	for i, number := range nums {
+		partnerNum := target - number
+
+		if partnerIndex, exists := seen[partnerNum]; exists {
+			return []int{partnerIndex, i}
 		}
-	}
 
-	return twoSum
+		seen[number] = i
+
+	}
+	return nil
 }
