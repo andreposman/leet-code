6227ecae6a887d5ee00d2709a7886f1c54b15534
Andre de Fazzio Posman <andre.posman@mercadolivre.com>
2026-01-20 23:43:01 -0300

    chore: improve initial state

--- hard/median-two-sorted-arr.go
+++ hard/median-two-sorted-arr.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	nums1 := []int{1, 3}
+	nums2 := []int{2}
+
+	result := findMedianSortedArrays(nums1, nums2)
+
+	fmt.Println(result)
+
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+
+	return 0.0
 }
